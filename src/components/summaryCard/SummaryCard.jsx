8819393428231a08f3e140f6cde2a436ba14a51b
Akanchha Sharma <akanchhasharma2310@gmail.com>
2025-07-17 23:06:21 +0530

    feat: summary card

--- src/components/summaryCard/SummaryCard.jsx
+++ src/components/summaryCard/SummaryCard.jsx
@@ -1,20 +1,32 @@
+import { MdTrendingDown, MdTrendingUp } from "react-icons/md";
 function SummaryCard({
-    label,
-    count,
-    percentageChange,
-    changeType,
-    type,
-    bgColor = '#dddddd',
+  label,
+  count,
+  percentageChange,
+  changeType,
+  prefix = "",
+  suffix = "",
+  bgColor,
 }) {
-    return <>
-        <div className="summaryCard flex flex-col" style={{backgroundColor: bgColor}}>
-            <div>{label}</div>
-            <div className="flex">
-                <span>{count}</span>
-                <span>{percentageChange + `%`}</span>
-            </div>
+  return (
+    <>
+      <div
+        className="summaryCard flex flex-col gap-2 p-6 rounded-2xl text-[#1C1C1C] min-w-[202px]"
+        style={{ backgroundColor: bgColor }}
+      >
+        <div className="text-sm/5 font-semibold rounded-xl hover:bg-[#0000000b]">
+          {label}
         </div>
+        <div className="flex gap-7 items-center justify-between rounded-xl hover:flex-row-reverse hover:bg-[#0000000b]">
+          <span className="text-2xl/9 font-semibold">{`${prefix}${count}${suffix}`}</span>
+          <span className="flex items-center text-xs/4.5">
+            {`${changeType === "positive" ? "+" : "-"}${percentageChange}%`}
+            {changeType === "positive" ? <MdTrendingUp /> : <MdTrendingDown />}
+          </span>
+        </div>
+      </div>
     </>
+  );
 }
 
 export default SummaryCard;
